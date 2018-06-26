4c2dbb5184e4f56d4c2193f33494ffcf50e016ee
SanamNewB <40501545+SanamNewB@users.noreply.github.com>
2018-06-26 16:58:25 -0700

    Update MainActivity.java

--- app/src/main/java/com/sanamshikalgar/audiblearabic/MainActivity.java
+++ app/src/main/java/com/sanamshikalgar/audiblearabic/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         TextView fruits = (TextView) findViewById(R.id.fruits);
         TextView numbers = (TextView) findViewById(R.id.numbers); // edit the .xml file to remove the onClick attribute for button click
         TextView phrases = (TextView) findViewById(R.id.phrases);
-        TextView things = (TextView) findViewById(R.id.things);
+        //TextView things = (TextView) findViewById(R.id.things);
 
         // Set onclick listener on those View
 
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        things.setOnClickListener(new View.OnClickListener() {
+//         things.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) { //onClick() is an abstract method which can have customized behavior
-                Intent thingsIntent = new Intent(MainActivity.this, ThingsActivity.class);
-                startActivity(thingsIntent);
-            }
-        });
+//             @Override
+//             public void onClick(View view) { //onClick() is an abstract method which can have customized behavior
+//                 Intent thingsIntent = new Intent(MainActivity.this, ThingsActivity.class);
+//                 startActivity(thingsIntent);
+//             }
+//         });
     }
 }
